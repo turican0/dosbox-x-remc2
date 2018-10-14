0217e33678accbcf9ea92eaace19af60648d1fdf
Tomáš Veselý <turican0@gmail.com>
2018-10-14 19:22:47 +0200

    181014-01

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -715,7 +715,7 @@ int engine_call(bool use32, Bitu selector, Bitu offset, Bitu oldeip) {
                 break;
                     
                 }
-            case 0x26e970: {
+            case 0x26e8f0: {
                 //saveactstate();
                 //if(xcounter>1)
                 DEBUG_EnableDebugger();
