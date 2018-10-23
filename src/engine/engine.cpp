0e9b5cd7564cb61e997cf4622ff0d81ddc73c091
Tomas Vesely <tomas.vesely@jansen-display.com>
2018-10-23 15:53:45 +0200

    181023-03

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -139,7 +139,7 @@ void enginestep() {
                 pause = false;
                 fprintf(fptestep, "AFTER 04X:%08X/%08X\n\n", SegValue(cs), reg_esp, reg_esp - 0x1E1000);
                 if (0x6F732F == oldmem)saveactstate();
-                DEBUG_EnableDebugger();
+                //DEBUG_EnableDebugger();
                 fclose(fptestep);
             }
     }
@@ -720,7 +720,7 @@ int engine_call(bool use32, Bitu selector, Bitu offset, Bitu oldeip) {
             case 0x287370: {
                 //saveactstate();
                 //if(xcounter>1)
-                DEBUG_EnableDebugger();
+                //DEBUG_EnableDebugger();
                 //xcounter++;
                 break;
             }
