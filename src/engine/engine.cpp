e187e6ae1ea4f88724dcd0304391d50d31b9be1d
Tomáš Veselý <turican0@gmail.com>
2018-11-03 12:47:21 +0100

    181103-02

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -747,7 +747,7 @@ int engine_call(bool use32, Bitu selector, Bitu offset, Bitu oldeip) {
                 //xcounter++;
                 break;
             }
-            case 0x25b110: {
+            case 0x225ee0: {
                 //saveactstate();
                 //if(xcounter>1)
                 DEBUG_EnableDebugger();
