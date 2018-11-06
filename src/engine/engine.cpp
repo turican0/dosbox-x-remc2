84c8d02900f618c7e812eed515ee2701a1dce4d0
Tomas Vesely <tomas.vesely@jansen-display.com>
2018-11-06 15:59:32 +0100

    181106-08

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -54,7 +54,7 @@ unsigned long findvarseg=0x168;
 //unsigned long findvaradr= 0x351660;
 //unsigned long findvaradr = 0xaaa355200;
 //unsigned long findvaradr = 0x19f0ec;
-unsigned long findvaradr = 0x2bb3bc;
+unsigned long findvaradr = 0x2bb3bc00;
 
 
 unsigned long prepreprepreprelastsel;
@@ -139,7 +139,7 @@ void enginestep() {
                 pause = false;
                 fprintf(fptestep, "AFTER 04X:%08X/%08X\n\n", SegValue(cs), reg_esp, reg_esp - 0x1E1000);
                 if (0x6F732F == oldmem)saveactstate();
-                DEBUG_EnableDebugger();
+                //DEBUG_EnableDebugger();
                 fclose(fptestep);
             }
     }
@@ -747,7 +747,7 @@ int engine_call(bool use32, Bitu selector, Bitu offset, Bitu oldeip) {
                 //xcounter++;
                 break;
             }
-            case 0x22aa30: {
+            case 0x225580: {
                 //saveactstate();
                 //if(xcounter>1)
                 DEBUG_EnableDebugger();
@@ -807,7 +807,7 @@ int engine_call(bool use32, Bitu selector, Bitu offset, Bitu oldeip) {
                     //restart_calls();
                     //saveactstate();
                     //findvaradr = 0x34eb54;
-                    DEBUG_EnableDebugger();
+                    //DEBUG_EnableDebugger();
                 }
                 //xcounter++;
                 break;
