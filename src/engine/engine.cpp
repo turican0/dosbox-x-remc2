ad06dabac5ed7b0822308629af6eb4cb113e4888
Tomáš Veselý <turican0@gmail.com>
2018-11-14 21:49:42 +0100

    181114-02

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -747,7 +747,7 @@ int engine_call(bool use32, Bitu selector, Bitu offset, Bitu oldeip) {
                 //xcounter++;
                 break;
             }
-            case 0x244c90: {
+            case 0x238730: {
                 //saveactstate();
                 //if(xcounter>1)
                 //oldmem = 0x12345678;
