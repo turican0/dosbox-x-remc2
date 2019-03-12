321f6944fd0033a530e9900e09b9d2ee7aec2fb9
turican0 <turican0@gmail.com>
2019-03-12 21:42:46 +0100

    190312-03

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -466,7 +466,7 @@ void enginestep() {
         //addprocedurestop(0x21f370, 0x5f, true, true, 0x400604, 0x2272a000);
         //addprocedurestop(0x238b20, 0x0, true, true, 0x3aa0a400 + 0xe051el, 0x2272a000);
         //addprocedurestop(0x20fcc0, 0x0, true, true, 0x35159c00, 0x2272a000);
-        addprocedurestop(0x236F70, 0x0, true, true, 0x38c5d4, 0x2272a000);
+        addprocedurestop(0x236F70, 0x0, true, true, 0x38c5d400, 0x2272a000);
         //addspy();
 
         sprintf(findname, "find-%04X-%08X.txt", findvarseg, findvaradr);
