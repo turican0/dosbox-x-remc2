e3632ba0da18ae44f91350449aee5cdb890fea81
turican0 <turican0@gmail.com>
2019-05-28 22:00:22 +0200

    190528-03

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -121,8 +121,9 @@ void writesequence(Bit32u codeadress, int count, int size, Bit32u dataadress) {
     //writesequencedataadress2 = dataadress2;
     //writesequencedataadress3 = dataadress3;
     sprintf(findnamex, "sequence-%08X-%08X.bin", codeadress, dataadress);
-    fopen_s(&fseq[lastwriteindexsequence], findnamex, "ab");
-    fclose(fseq[lastwriteindexsequence]);
+    remove(findnamex);
+    //fopen_s(&fseq[lastwriteindexsequence], findnamex, "ab");
+    //fclose(fseq[lastwriteindexsequence]);
     lastwriteindexsequence++;
 }
 
@@ -328,8 +329,8 @@ void enginestep() {
 
         //writesequence(0x268610, 0x300, 0xb0, 0x3514b0, 0, 0);
 
-        writesequence(0x00238CF0, 0x38, 0x70000, 0x2dc4e0);
-        writesequence(0x00238CF0, 0x38, 0x36e16, 0x356038);
+        writesequence(0x00218240, 0x110, 0x70000, 0x2dc4e0);
+        writesequence(0x00218240, 0x110, 0x36e16, 0x356038);
         
         //addprocedurestop(0x235a50, 0x0, true, true, 0x358ffc00 + 0x333);
         //addprocedurestop(0x236F70, 0x0, true, true, 0x35932f);
